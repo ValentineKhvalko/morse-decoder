a37720f840cf2b35c1843f6de995d8451ff70b48
Valentine <esfeeder@gmail.com>
2020-03-03 11:24:12 +0300

    fix: simplified the code

--- src/index.js
+++ src/index.js
@@ -40,22 +40,22 @@ const MORSE_TABLE = {
 function decode(expr) {
     let start = 0;
     let decodedStr = '';
-    for (let i = 0; i < expr.length/10; i++) {
+    for( let i = 0; i < expr.length/10; i++) {
         let morse = '';
-        let decodeLetter = expr.slice(start, start + 10);
-        start+=10;
-        if(decodeLetter === '**********') {
+        let decodedLetter = expr.slice(start, start+10);
+        start +=10;
+        if(decodedLetter === '**********'){
             decodedStr += ' ';
-            continue;
+            continue; 
         }
-        for (let j = 0; j < 10; j += 2) {
-            if(decodeLetter.slice(j, j+2) === "10") {
+        for(j = 0; j < decodedLetter.length; j+=2) {
+            if(decodedLetter.slice(j, j+2) === '10') {
                 morse += '.';
-            } else if(decodeLetter.slice(j, j+2) === '11') {
+            } else if(decodedLetter.slice(j, j+2) === '11'){
                 morse += '-';
             }
         }
-        decodedStr += MORSE_TABLE[morse.split('').join('')];
+        decodedStr += MORSE_TABLE[morse]; 
     }
     return decodedStr;
 }
